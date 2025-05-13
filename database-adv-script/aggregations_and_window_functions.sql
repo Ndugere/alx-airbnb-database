a295616ef52970235815efcd826f20d5a2debba9
Your Name <you@example.com>
2025-05-14 02:04:09 +0300

    Apply Aggregations and Window Functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,4 +1,19 @@
-SELECT u.user_id, u.first_name, u.last_name, COUNT(b.booking_id) AS total_bookings
+-- Query 1: Find total bookings made by each user
+SELECT 
+    u.user_id,                            -- Select the user ID
+    u.first_name,                         -- Select the first name of the user
+    u.last_name,                          -- Select the last name of the user
+    COUNT(b.booking_id) AS total_bookings -- Count the total number of bookings for each user
 FROM users u
-JOIN bookings b ON u.user_id = b.user_id
-GROUP BY u.user_id;
+JOIN bookings b ON u.user_id = b.user_id  -- Join the 'users' table with the 'bookings' table on user_id
+GROUP BY u.user_id;                      -- Group the results by user_id to count bookings for each user
+
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id
+ORDER BY rank;
