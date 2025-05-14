69043d196b5fcc2b60d05997c4fd03fc89bc8cf6
Your Name <you@example.com>
2025-05-14 15:10:44 +0300

    an initial query that retrieves all bookings along with the user details, property details, and payment details

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,17 +1,19 @@
+EXPLAIN ANALYZE
 SELECT 
   b.booking_id, 
   u.user_first_name, 
   p.property_name, 
   p.property_location, 
-  payments.amount 
+  pay.amount
 FROM 
   bookings AS b
-INNER JOIN users AS u 
-  ON b.user_id = u.user_id
-INNER JOIN properties AS p 
-  ON p.property_id = b.property_id
-INNER JOIN payments 
-  ON payments.booking_id = b.booking_id;
+JOIN users AS u ON b.user_id = u.user_id
+JOIN properties AS p ON b.property_id = p.property_id
+JOIN payments AS pay ON pay.booking_id = b.booking_id
+WHERE 
+  b.start_date IS NOT NULL
+  AND pay.amount > 0;
+
 
 
 
@@ -30,3 +32,5 @@ INNER JOIN properties AS p
   ON p.property_id = b.property_id
 INNER JOIN payments 
   ON payments.booking_id = b.booking_id;
+
+
